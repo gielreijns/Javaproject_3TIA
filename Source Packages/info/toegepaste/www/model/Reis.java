78f7035d0279a916f7b52730ec41bba6db7ed7e4
Bruno <Bruno@Bruno-Laptop.khk.be>
2013-10-14 15:29:51 +0200

    boeking pagina

---
+++ Source Packages/info/toegepaste/www/model/Reis.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.toegepaste.www.model;
+
+import java.util.List;
+/**
+ *
+ * @author Bruno
+ */
+public interface Reis {
+    
+}
