135d6c5b70191f7f6192246cc9b9c29a8c64da2c
Bruno <Bruno@Bruno-Laptop>
2013-11-04 09:41:08 +0100

    aanpassen klasse

--- Source Packages/info/toegepaste/www/model/Land.java
+++ Source Packages/info/toegepaste/www/model/Land.java
@@ -4,15 +4,42 @@
  */
 package info.toegepaste.www.model;
 
+import java.io.Serializable;
 import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 /**
  *
  * @author Bruno
  */
-public class Land {
-    private Long id;
+@Entity
+@Table(name = "land")
+@NamedQueries({
+
+})
+public class Land implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "naam")
     private String naam;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "continent")
     private List<Continent> continenten;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "gemeente")
     private List<Gemeente> gemeenten;
 
     public Land() {
@@ -26,11 +53,11 @@ public class Land {
         this.gemeenten = gemeenten;
     }
     
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -53,5 +80,18 @@ public class Land {
     @Override
     public String toString() {
         return "Land{" + "id=" + id + ", naam=" + naam + ", continenten=" + continenten + ", gemeenten=" + gemeenten + '}';
+    }
+    
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Land)) {
+            return false;
+        }
+        Land other = (Land) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
     }
 }
