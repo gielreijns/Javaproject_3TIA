6d73c0c10394c663decb8bf8a6f5d42a4efac7a3
Bruno <Bruno@Bruno-Laptop>
2013-11-04 09:47:43 +0100

    aanpassen klasse

--- Source Packages/info/toegepaste/www/model/Reis.java
+++ Source Packages/info/toegepaste/www/model/Reis.java
@@ -4,34 +4,81 @@
  */
 package info.toegepaste.www.model;
 
+import java.io.Serializable;
 import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 /**
  *
  * @author Bruno
  */
-public class Reis {
-    private Long id;
+@Entity
+@Table(name = "reis")
+@NamedQueries({
+
+})
+public class Reis implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "naam")
     private String naam;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "beschrijving")
     private String beschrijving;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "vertrekdatum")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date vertrekdatum;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "terugkeerdatum")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date terugkeerdatum;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "aantalPlaatsen")
     private int aantalPlaatsen;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "prijsPerPersoon")
     private Double prijsPerPersoon;
-    private Long gemeenteId;
-    private Long vervoerswijzeId;
-    private Long vertrekplaatsId;
+    @JoinColumn(name = "gemeenteId", referencedColumnName = "id")
+    @ManyToOne(optional = false)
     private Gemeente gemeente;
+    @JoinColumn(name = "vervoerswijzeId", referencedColumnName = "id")
+    @ManyToOne(optional = false)
     private Vervoerswijze vervoerswijze;
+    @JoinColumn(name = "vertrekplaatsId", referencedColumnName = "id")
+    @ManyToOne(optional = false)
     private Vertrekplaats vertrekplaats;
 
     public Reis() {
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -83,30 +130,6 @@ public class Reis {
         this.prijsPerPersoon = prijsPerPersoon;
     }
 
-    public Long getGemeenteId() {
-        return gemeenteId;
-    }
-
-    public void setGemeenteId(Long gemeenteId) {
-        this.gemeenteId = gemeenteId;
-    }
-
-    public Long getVervoerswijzeId() {
-        return vervoerswijzeId;
-    }
-
-    public void setVervoerswijzeId(Long vervoerswijzeId) {
-        this.vervoerswijzeId = vervoerswijzeId;
-    }
-
-    public Long getVertrekplaatsId() {
-        return vertrekplaatsId;
-    }
-
-    public void setVertrekplaatsId(Long vertrekplaatsId) {
-        this.vertrekplaatsId = vertrekplaatsId;
-    }
-
     public Gemeente getGemeente() {
         return gemeente;
     }
@@ -133,7 +156,7 @@ public class Reis {
 
     @Override
     public String toString() {
-        return "Reis{" + "id=" + id + ", naam=" + naam + ", beschrijving=" + beschrijving + ", vertrekdatum=" + vertrekdatum + ", terugkeerdatum=" + terugkeerdatum + ", aantalPlaatsen=" + aantalPlaatsen + ", prijsPerPersoon=" + prijsPerPersoon + ", gemeenteId=" + gemeenteId + ", vervoerswijzeId=" + vervoerswijzeId + ", vertrekplaatsId=" + vertrekplaatsId + ", gemeente=" + gemeente + ", vervoerswijze=" + vervoerswijze + ", vertrekplaats=" + vertrekplaats + '}';
+        return "Reis{" + "id=" + id + ", naam=" + naam + ", beschrijving=" + beschrijving + ", vertrekdatum=" + vertrekdatum + ", terugkeerdatum=" + terugkeerdatum + ", aantalPlaatsen=" + aantalPlaatsen + ", prijsPerPersoon=" + prijsPerPersoon + ", gemeente=" + gemeente + ", vervoerswijze=" + vervoerswijze + ", vertrekplaats=" + vertrekplaats + '}';
     }
     
     
